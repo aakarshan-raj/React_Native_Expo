134c5d58748e4336c4046f49abeb0fa88209868d
aakarshan-raj <aakarshan7raj@gmail.com>
2023-05-31 22:38:09 +0530

    running on android emulator

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First Expo App</Text>
+      <Text>Running on Emulator This is nice</Text>
       <StatusBar style="auto" />
     </View>
   );
