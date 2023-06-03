faf9054f3a434cffee1d8402314674f317d91cbf
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-03 18:27:15 +0530

    looks like a good note app

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text, TextInput, Button,StyleSheet} from "react-native"
+import { useState } from "react";
 
-export default function App() {
+ export default App = ()=> {
+  
+  const [value,updateValue] = useState('');
+  const [goals,setGoals] = useState([]);
+  function grabInput(input_data){
+      updateValue(input_data)
+  };
+
+  function addGoalHandler(){
+    setGoals((currentGoals)=>[...currentGoals,value])
+    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Running on Emulator This is nice</Text>
-      <StatusBar style="auto" />
+
+    
+    <View style={styles.appContainer}>
+        <View style={styles.inputContainer}> 
+             <TextInput style={styles.TextInput} placeholder="Write something" onChangeText={grabInput}/>
+             <Button title="Add" onPress={addGoalHandler}/>
+        </View>
+        <View style={styles.goalContainer}>
+            {goals.map((item,item_number)=><Text key={item_number}>{item_number} {item}</Text>)}
+         </View>
     </View>
+
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+{
+ appContainer:{
+  flex:1,
+  padding:50,
+ },
+ inputContainer:{
+  flex:1,
+ flexDirection:"row",
+ alignItems:'center',
+ justifyContent:'space-between',
+ marginBottom:20,
+ borderBottomWidth:2
+ 
+
+ },
+ TextInput:{
+  padding:8,
+   borderColor:'#2aff',
+   width:"80%"
+ },
+ goalContainer:{
+  flex:5,
+ }
+
+}
+);
